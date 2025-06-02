4cec89d76dfceb0ad48f258572e255f9ba91dc39
Javier Fernández <planetazuzu@gmail.com>
2025-06-02 19:14:05 +0000

    podemos quitar la pagina a la que hace mencion

--- src/app/gobiernodelarioja-crm/page.tsx
+++
@@ -1,8 +0,0 @@
-// Esto podría ser una página de bienvenida o redirigir al dashboard.
-// Por ahora, redirigiremos al dashboard.
-import { redirect } from 'next/navigation';
-
-export default function CrmRootPage() {
-  redirect('/gobiernodelarioja-crm/dashboard');
-  return null; // La redirección ocurre en el servidor, esto no se renderizará.
-}
